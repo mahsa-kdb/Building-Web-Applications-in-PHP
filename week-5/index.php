f4c99cc3292462c68b52227d3325c8653f05b2a0
Mahsa Khodabakhsh <mahsan.khodabakhsh@gmail.com>
2021-08-06 17:05:28 +0430

    Introduction to PHP was completed.

---
+++ week-5/index.php
@@ -0,0 +1,19 @@
+<title>Fatemeh Khodabakhsh PHP</title>
+<h1>Fatemeh Khodabakhsh PHP</h1>
+<p><?php
+	print "The SHA256 hash of \"Fatemeh Khodabakhsh\" is ".hash('sha256', 'Fatemeh Khodabakhsh');
+?></p>
+<pre>ASCII ART:
+    ***************
+    ***************
+    **
+    **
+    ********
+    ********
+    **
+    **
+    **
+</pre>
+<a href="fail.php">Click here to check the error setting.</a>
+<br>
+<a href="check.php">Click here to cause a traceback.</a>
